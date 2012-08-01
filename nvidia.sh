f89a1ac8f0ad1422278338653e1baf627707bde5
Parker <parker@debian>
2012-08-01 01:35:41 -0400

    Added comments describing the commands

--- nvidia.sh
+++ nvidia.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+#Downloads and installs module-assistant and nvidia-kernel-common
 apt-get install module-assistant nvidia-kernel-common &&
+#Downloads and installs the files for you current kernel version.
 m-a auto-install nvidia-kernel${VERSION}-source &&
+#Downloads drivers and utilities specific to yorur kernel version.
 apt-get install nvidia-glx${VERSION} &&
+#Creates a basic xorg.conf file for X to load.
 cat > /etc/X11/xorg.conf << EOF
 Section "Module"
     Load        "glx"
