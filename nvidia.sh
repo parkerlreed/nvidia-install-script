34defb71578e3eddadbdbed8f9bbaa6ccfde3c43
parkerlreed <parker.l.reed@gmail.com>
2012-11-25 19:37:11 -0500

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -3,7 +3,7 @@
 apt-get install module-assistant nvidia-kernel-common &&
 #Downloads and installs the files for you current kernel version.
 m-a auto-install nvidia-kernel${VERSION}-source &&
-#Downloads drivers and utilities specific to yorur kernel version.
+#Downloads drivers and utilities specific to your kernel version.
 apt-get install nvidia-glx${VERSION} &&
 #Creates a basic xorg.conf file for X to load.
 cat > /etc/X11/xorg.conf << EOF
